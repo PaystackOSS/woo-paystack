dfff15035cb7e2ea579df6dfa354b1255b9ff09a
Tunbosun Ayinla <tayinla@gmail.com>
2026-06-10 19:43:39 +0400

    Add support for refunds in custom gateways

--- includes/custom-gateways/gateway-four/class-wc-gateway-paystack-four.php
+++ includes/custom-gateways/gateway-four/class-wc-gateway-paystack-four.php
@@ -66,6 +66,7 @@ class WC_Gateway_Paystack_Four extends WC_Gateway_Custom_Paystack {
 
 		$this->supports = array(
 			'products',
+			'refunds',
 			'tokenization',
 			'subscriptions',
 			'multiple_subscriptions',
--- includes/custom-gateways/gateway-two/class-wc-gateway-paystack-two.php
+++ includes/custom-gateways/gateway-two/class-wc-gateway-paystack-two.php
@@ -66,6 +66,7 @@ class WC_Gateway_Paystack_Two extends WC_Gateway_Custom_Paystack {
 
 		$this->supports = array(
 			'products',
+			'refunds',
 			'tokenization',
 			'subscriptions',
 			'multiple_subscriptions',
